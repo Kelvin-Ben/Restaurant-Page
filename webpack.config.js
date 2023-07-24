0d49ac053ec0fe27d353e7b115f8c6c56d51fa87
Kelvin-Ben <kevoben14@gmail.com>
2023-07-24 18:38:48 +0300

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: "development",
+  module: {
+    rules: [
+      // ... your other rules ...
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      hot: true
+    },
+    port: 8080,
+  },
+};
